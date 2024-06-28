da5ccd55ff4067994638e8e152a6c72c387c46a3
Rice <112427358+ricefarmernft@users.noreply.github.com>
2024-06-28 15:54:28 -0500

    created footer

---
+++ app/components/footerPage.js
@@ -0,0 +1,15 @@
+const FooterPage = () => {
+  return (
+    <footer>
+      <div className="text-center text-sm text-gray-400 p-2">
+        © 2024 Tonkeeper. Made by{" "}
+        <a href="https://x.com/RiceFarmerNFT" target="_blank" className="link">
+          ricefarmer
+        </a>
+        .
+      </div>
+    </footer>
+  );
+};
+
+export default FooterPage;
